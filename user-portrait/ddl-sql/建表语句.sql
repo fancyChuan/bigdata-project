e5f7768e8c0960f5e1840ac1fe973d688482e61b
hp-fancy <1247375074@qq.com>
2021-05-09 23:00:49 +0800

    用户画像：建表语句修改

--- user-portrait/ddl-sql/建表语句.sql
+++ user-portrait/ddl-sql/建表语句.sql
@@ -2,8 +2,8 @@
 drop table if exists dmp_book.userprofile_userlabel_map_all
 create table `dmp_book.userprofile_userlabel_map_all`(
     `userid` string comment 'userid',
-    `userlabels` map<string,string> comment 'tagsmap',)
-comment 'userid 用户标签汇聚'
+    `userlabels` map<string,string> comment 'tagsmap'
+) comment 'userid 用户标签汇聚'
 partitioned by ( `data_date` string comment '数据日期')
 ;
 
