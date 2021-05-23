380d1dff1b051de42655214d3774395af6fd8d6d
hp-fancy <1247375074@qq.com>
2021-05-24 00:25:03 +0800

    电商数仓1.0：flume拦截器配置和启动脚本

--- shop-warehouse/flume-interceptor/bin/fl.sh
+++ shop-warehouse/flume-interceptor/bin/fl.sh
@@ -2,18 +2,18 @@
 
 case $1 in
 "start"){
-    for i in s02 s03
-    do
-        echo " --------启动 $i 采集flume-------"
-        ssh $i "nohup /usr/local/flume/bin/flume-ng agent --conf-file /data/apps/eb-wh/flume/file-flume-kafka.conf --name a1 -Dflume.root.logger=INFO,LOGFILE >/dev/null 2>&1 &"
-    done
+        for i in hadoop102 hadoop103
+        do
+                echo " --------启动 $i 采集flume-------"
+                ssh $i "nohup /usr/local/flume/bin/flume-ng agent --conf-file /home/appuser/forlearn/shop-wh/flumeconf --name a1 -Dflume.root.logger=INFO,LOGFILE >/home/appuser/forlearn/shop-wh/flumetest1 2>&1  &"
+        done
 };;
 "stop"){
-    for i in s02 s03
-    do
-        echo " --------停止 $i 采集flume-------"
-        ssh $i "ps -ef | grep file-flume-kafka | grep -v grep |awk '{print \$2}' | xargs kill"
-    done
+        for i in hadoop102 hadoop103
+        do
+                echo " --------停止 $i 采集flume-------"
+                ssh $i "ps -ef | grep file-flume-kafka | grep -v grep |awk  '{print \$2}' | xargs kill"
+        done
 
 };;
 esac
