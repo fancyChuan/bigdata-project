7d801f6858ed97cc728d475da2bb357d260d80eb
hp-fancy <1247375074@qq.com>
2021-10-30 15:45:35 +0800

    flink实时数仓：更新logger模块和pom文件

---
+++ shop-warehouse-realtime/shop-logger/src/main/java/cn/fancychuan/shoplogger/ShopLoggerApplication.java
@@ -0,0 +1,13 @@
+package cn.fancychuan.shoplogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopLoggerApplication.class, args);
+    }
+
+}
